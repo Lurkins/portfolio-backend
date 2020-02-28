1c6f9b5ea04b81fb75adb6361ebe06a82c425707
Paul Perkins <paulperkins7@gmail.com>
2020-02-28 11:37:25 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,216 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
+const app = express()
+
+require('dotenv').config()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//Test route
+app.get('/api/test', (req, res) => {
+  res.send("Success!");
+});
+
+app.post('/api/form', (req, res) => {
+    console.log(req.body);
+    let transporter = nodemailer.createTransport({
+        host: process.env.HOST,
+        port: process.env.MAILPORT,
+        secure: process.env.SECURE,
+        auth: {
+            type: process.env.TYPE,
+            user: process.env.MAILUSER,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            refreshToken: process.env.REFRESH_TOKEN,
+            accessToken: process.env.ACCESS_TOKEN
+        }
+    });
+    const output = `<!doctype html>
+    <html>
+      <head>
+        <meta name="viewport" content="width=device-width">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Portfolio Contact Email</title>
+        <style>
+    @media only screen and (max-width: 620px) {
+      table[class=body] h1 {
+        font-size: 28px !important;
+        margin-bottom: 10px !important;
+      }
+    
+    table[class=body] p,
+    table[class=body] ul,
+    table[class=body] ol,
+    table[class=body] td,
+    table[class=body] span,
+    table[class=body] a {
+        font-size: 16px !important;
+      }
+    
+      table[class=body] .wrapper,
+    table[class=body] .article {
+        padding: 10px !important;
+      }
+    
+      table[class=body] .content {
+        padding: 0 !important;
+      }
+    
+      table[class=body] .container {
+        padding: 0 !important;
+        width: 100% !important;
+      }
+    
+      table[class=body] .main {
+        border-left-width: 0 !important;
+        border-radius: 0 !important;
+        border-right-width: 0 !important;
+      }
+    
+      table[class=body] .btn table {
+        width: 100% !important;
+      }
+    
+      table[class=body] .btn a {
+        width: 100% !important;
+      }
+    
+      table[class=body] .img-responsive {
+        height: auto !important;
+        max-width: 100% !important;
+        width: auto !important;
+      }
+    }
+    @media all {
+      .ExternalClass {
+        width: 100%;
+      }
+    
+      .ExternalClass,
+    .ExternalClass p,
+    .ExternalClass span,
+    .ExternalClass font,
+    .ExternalClass td,
+    .ExternalClass div {
+        line-height: 100%;
+      }
+    
+      .apple-link a {
+        color: inherit !important;
+        font-family: inherit !important;
+        font-size: inherit !important;
+        font-weight: inherit !important;
+        line-height: inherit !important;
+        text-decoration: none !important;
+      }
+    
+      #MessageViewBody a {
+        color: inherit;
+        text-decoration: none;
+        font-size: inherit;
+        font-family: inherit;
+        font-weight: inherit;
+        line-height: inherit;
+      }
+    
+      .btn-primary table td:hover {
+        background-color: #34495e !important;
+      }
+    
+      .btn-primary a:hover {
+        background-color: #ffc107 !important;
+        border-color: #34495e !important;
+        color: #000000 !important;
+      }
+    }
+    </style>
+      </head>
+      <body class="" style="background-color: #f6f6f6; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;">
+        <span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;"></span>
+        <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f6f6f6; width: 100%;" width="100%" bgcolor="#f6f6f6">
+          <tr>
+            <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;" valign="top">&nbsp;</td>
+            <td class="container" style="font-family: sans-serif; font-size: 14px; vertical-align: top; display: block; max-width: 580px; padding: 10px; width: 580px; margin: 0 auto;" width="580" valign="top">
+              <div class="content" style="box-sizing: border-box; display: block; margin: 0 auto; max-width: 580px; padding: 10px;">
+    
+                <!-- START CENTERED BLACK CONTAINER -->
+                <table role="presentation" class="main" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background: #ffffff; border-radius: 3px; width: 100%;" width="100%">
+    
+                  <!-- START MAIN CONTENT AREA -->
+                  <tr>
+                    <td class="wrapper" style="font-family: sans-serif; font-size: 14px; vertical-align: top; box-sizing: border-box; padding: 20px;" valign="top">
+                      <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;" width="100%">
+                        <tr>
+                          <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;" valign="top">
+                            <h1 style="font-family: sans-serif; line-height: 1.4; margin: 0; margin-bottom: 30px; color: #491B8F; font-size: 35px; font-weight: 300; text-align: left; text-transform: capitalize;">Hi Paul,</h1>
+                            <h3 style="color: #3d3d3d; font-family: sans-serif; font-weight: 400; line-height: 1.4; margin: 0; margin-bottom: 30px;">You have a message from ${req.body.name}. Email: ${req.body.email}.</h3>
+                            <p style="color: #3d3d3d; font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;">Message:<br>${req.body.message}</p>
+                            <p style="color: #3d3d3d; font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;">Cheers!</p>
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+    
+                <!-- END MAIN CONTENT AREA -->
+                </table>
+                <!-- END CENTERED WHITE CONTAINER -->
+    
+                <!-- START FOOTER -->
+                <div class="footer" style="clear: both; margin-top: 10px; text-align: center; width: 100%;">
+                  <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;" width="100%">
+                    <tr>
+                      <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; color: #999999; font-size: 12px; text-align: center;" valign="top" align="center">
+                        <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">Perkfolio, Austin TX</span>
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+                <!-- END FOOTER -->
+    
+              </div>
+            </td>
+            <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;" valign="top">&nbsp;</td>
+          </tr>
+        </table>
+      </body>
+    </html>`
+
+    const mailOptions = {
+      from: `${req.body.email}`,
+      to: 'perkfolio@gmail.com',
+      subject: `Portfolio message from ${req.body.name}`,
+      html: output,
+      replyTo: `${req.body.email}`
+    }
+    transporter.sendMail(mailOptions, function(err, info) {
+      if (err) {
+        res.send({
+            type: 'error',
+            msg: 'There was an error sending your message.'
+          });
+        console.error('There was an error: ', err);
+      } else {
+        console.log("info.messageId: " + info.messageId);
+        console.log("info.envelope: " + info.envelope);
+        console.log("info.accepted: " + info.accepted);
+        console.log("info.rejected: " + info.rejected);
+        console.log("info.pending: " + info.pending);
+        console.log("info.response: " + info.response);
+        res.send({
+            type: 'success',
+            msg: "Message sent! I'll email you back as soon as I can."
+          });
+      }
+      transporter.close();
+    })
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
